81fea2040297d5b1f4561b247f6b88b2fcb79fef
German Feskov <g.feskov@yandex.ru>
2025-04-28 21:48:41 +0300

    clear migrations

--- migrations/create_tokens_table.sql
+++ migrations/create_tokens_table.sql
@@ -8,9 +8,3 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     expired_at TIMESTAMP DEFAULT now() + interval '7 days',
     is_used BOOLEAN DEFAULT false
 );
-
-INSERT INTO refresh_tokens (user_id, refresh_token_hash, client_ip)
-VALUES
-('11111111-1111-1111-1111-111111111111', crypt('test_refresh_hash_1', gen_salt('bf')), '127.0.0.1'),
-('22222222-2222-2222-2222-222222222222', crypt('test_refresh_hash_2', gen_salt('bf')), '127.0.0.1');
-
